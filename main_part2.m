abc46f3379b8d16e787535cb6a803ef6bd40594c
magnushs <magnus.skeide@gmail.com>
2014-03-16 16:50:07 +0100

    Fullførelse
    Fosskokt presentasjon. Lever nå.

--- main_part2.m
+++ main_part2.m
@@ -8,7 +8,7 @@ function decodedNumber = main_part2(ToneGeneratedPart1)
     
     toneLenght = Fs*0.2; %Lengden av en tone er samplefrekvens * tid
     pauseLenght = Fs*0.05; %Lengden av pausen er samplefrekvens * tid
-    numbersToFilter = round(length(ToneGeneratedPart1)/(toneLenght + pauseLenght)); %Finner ut hvor mange nummer signalet består av
+    numbersToFilter = round(length(ToneGeneratedPart1)/(toneLenght + pauseLenght)); %Finner ut hvor mange nummer signalet best?r av
     toFilter = [];
     start = 1;
     for n = 1:numbersToFilter
@@ -16,10 +16,10 @@ function decodedNumber = main_part2(ToneGeneratedPart1)
         start = start + toneLenght + pauseLenght;
     end
     
-    %% Lager Båndpassfiltrene som blir brukt til å identifisere tallene
+    %% Lager B?ndpassfiltrene som blir brukt til ? identifisere tallene
     
     Frekvenser = [697 770 852 941 1209 1336 1477]; % De forskjellige frekvensene i bruk i DTMF
-    L = 300; % Bredden på båndet til båndpassfilteret
+    L = 300; % Bredden p? b?ndet til b?ndpassfilteret
     n = 0:L-1; 
     filters = [];   
     for i = 1:length(Frekvenser)
@@ -36,27 +36,27 @@ function decodedNumber = main_part2(ToneGeneratedPart1)
     xlabel('Frekvens');
     ylabel('|H(w)|');
     
-    %% Deler opp signalet til hvert enkelt tall og Dekoder input signalet ved bruk av Båndpassfilterene tidligere laget
+    %% Deler opp signalet til hvert enkelt tall og Dekoder input signalet ved bruk av B?ndpassfilterene tidligere laget
     
     for k = 1:(length(toFilter)/(Fs*0.2))
         NumberToFilter = toFilter((k-1)*(Fs*0.2)+1:k*(Fs*0.2));
         threshold = 0.8;
-        % Siden filteret ikke er perfekt, setter vi grensen på 0.85.
-        % Hvis vi får et treff høyere enn 0.85 så antar vi derfor at
+        % Siden filteret ikke er perfekt, setter vi grensen p? 0.85.
+        % Hvis vi f?r et treff h?yere enn 0.85 s? antar vi derfor at
         % frekvensen finnes.
-        temp = []; %Liste for å lagre frekvensene vi finner.
+        temp = []; %Liste for ? lagre frekvensene vi finner.
         for n=1:7
             y = filter(filters(n, 1:L), 1, NumberToFilter); %Filtrerer vektoren part gjennom filteret
-            if(max(y) > threshold) % Hvis amplituden på signalet er høyere enn thresholdet, så registreres det.
+            if(max(y) > threshold) % Hvis amplituden p? signalet er h?yere enn thresholdet, s? registreres det.
                 temp = [temp n]; % Legger til frekvenstallet i listen
             end
         end
         
     
     
-        %% Summerer frekvensene og finner ut hvilke sum som hører til hvilket tall
+        %% Summerer frekvensene og finner ut hvilke sum som h?rer til hvilket tall
         sum = Frekvenser(temp(1)) + Frekvenser(temp(2)); %Henter ut de to dekodede frekvensene fra parten og Summerer de to frekvensene
-        %Finner ut hvilken sum som tilhører hvilket tall
+        %Finner ut hvilken sum som tilh?rer hvilket tall
         switch sum
             case 1906
                 decodedNumber = [decodedNumber '1'];
